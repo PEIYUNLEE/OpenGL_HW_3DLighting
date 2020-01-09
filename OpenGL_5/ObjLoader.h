a52d3e9c90b7bb80fbffee3ce6125cfcc1299f66
PEI-YUN LEE <may1092200258@gmail.com>
2020-01-10 04:43:30 +0800

    Revert "Initial commit"
    This reverts commit 5785ef84535bb34134072932543af588ec69a63e.

--- OpenGL_5/ObjLoader.h
+++
@@ -1,9 +0,0 @@
-#ifndef OBJLOADER_H_
-#define OBJLOADER_H_
-
-#include <vector>
-#include "header/Angel.h"
-
-bool LoadObj(const char*  path, std::vector<float> *vertices, std::vector<float> *normals, char type);
-
-#endif
